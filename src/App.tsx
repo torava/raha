bbdee52367399626cbb2f3dc120def14bcf3f189
Teemu Orava <teemu.orava@kapsi.fi>
2026-06-09 16:07:01 +0300

    Add children 1h

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Box, Button, TextField } from '@mui/material';
-import type { PieValueType } from '@mui/x-charts';
+import type { PieItemId, PieValueType } from '@mui/x-charts';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { useState } from 'react';
 
 function App() {
   const [data, setData] = useState<PieValueType[]>([]);
+  const [childData, setChildData] = useState<(PieValueType & { parentId?: PieItemId })[]>([]);
+  const middleRadius = 167;
   return (
     <>
       <PieChart
-        series={[{ data }]}
+        series={[
+          { data, innerRadius: 0, outerRadius: middleRadius, cornerRadius: 3 },
+          { data: childData, innerRadius: middleRadius, outerRadius: middleRadius + 20, cornerRadius: 3 },
+        ]}
         width={375}
         height={375}
         slotProps={{
@@ -19,28 +24,67 @@ function App() {
         }}
       />
       <Box sx={{ textAlign: 'center' }}>
-        {data.map((item, index) => <Box key={index}>
-          <TextField
-            label="Label"
-            value={item.label}
-            onChange={(event) => {
-              const newData = [...data];
-              newData[index] = { ...item, label: event.target.value };
-              setData(newData);
-            }}
-            sx={{ m: 1 }}
-          />
-          <TextField
-            label="Value"
-            onChange={(event) => {
-              const newData = [...data];
-              newData[index] = { ...item, value: Number(event.target.value) };
-              setData(newData);
-            }}
-            sx={{ m: 1 }}
-          />
-        </Box>)}
-        <Button onClick={() => setData([...data, { id: data.length, value: 0 }])} disableRipple>Add</Button>
+        {data.map((item, index) => (
+          <Box key={index}>
+            <Box>
+              <TextField
+                label="Label"
+                value={item.label}
+                onChange={(event) => {
+                  const newData = [...data];
+                  newData[index] = { ...item, label: event.target.value };
+                  setData(newData);
+                }}
+                sx={{ m: 1 }}
+              />
+              <TextField
+                label="Value"
+                onChange={(event) => {
+                  const newData = [...data];
+                  newData[index] = { ...item, value: Number(event.target.value) };
+                  setData(newData);
+                }}
+                sx={{ m: 1 }}
+              />
+            </Box>
+            {childData
+              .filter((child) => child.parentId === item.id)
+              .map((child) => (
+                <Box>
+                  <TextField
+                    label="Label"
+                    value={child.label}
+                    onChange={(event) => {
+                      const newChildData = [...childData];
+                      const childIndex = newChildData.findIndex((c) => c.id === child.id);
+                      newChildData[childIndex] = { ...child, label: event.target.value };
+                      setChildData(newChildData);
+                    }}
+                    sx={{ m: 1 }}
+                  />
+                  <TextField
+                    label="Value"
+                    onChange={(event) => {
+                      const newChildData = [...childData];
+                      const childIndex = newChildData.findIndex((c) => c.id === child.id);
+                      newChildData[childIndex] = { ...child, value: Number(event.target.value) };
+                      setChildData(newChildData);
+                    }}
+                    sx={{ m: 1 }}
+                  />
+                </Box>
+              ))}
+            <Button
+              onClick={() => setChildData([...childData, { parentId: item.id, id: childData.length, value: 0 }])}
+              disableRipple
+            >
+              Add Child
+            </Button>
+          </Box>
+        ))}
+        <Button onClick={() => setData([...data, { id: data.length, value: 0 }])} disableRipple>
+          Add
+        </Button>
       </Box>
     </>
   );
